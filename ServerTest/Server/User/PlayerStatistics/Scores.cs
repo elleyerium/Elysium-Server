9d4b6ec0656bef48feec6505a11b981450ec06a4
Elleyer <shudenkoslavik@gmail.com>
2019-04-29 15:35:06 +0300

    Get and Set scores from/to db.

--- ServerTest/Server/User/PlayerStatistics/Scores.cs
+++ ServerTest/Server/User/PlayerStatistics/Scores.cs
@@ -5,14 +5,27 @@ using System.Text;
 using System.Threading.Tasks;
 using ServerTest.Database;
 using ServerTest.Server;
+using ServerTest.Database.DataTypes;
 
 namespace ServerTest.User.PlayerStatistics
 {
     class Scores
     {
-        public void GetUserScores(string username)
+        public static void CreateTable(string username)
         {
-            //RequestToDB();
+            var data = INSERT.InsertRequest("scores", "username", username);
+            RequestToDB.CreateRequest(data);
+
+        }
+        public static void GetUserScores(string username)
+        {
+            RequestToDB.CreateRequest(SELECT.SelectLoginRequest("score","scores","username",Items.GetScoreList(username)));
+        }
+
+        public static void SetUserScores(string data)
+        {
+            var request = INSERT.InsertRequest($"scores", "score", data);
+            RequestToDB.CreateRequest(request);
         }
     }
 }
