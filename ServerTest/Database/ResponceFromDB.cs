37537e8289c185520294ae0893fa3850c7366d84
Elleyer <shudenkoslavik@gmail.com>
2019-04-29 15:34:16 +0300

    New cases for exceptions

--- ServerTest/Database/ResponceFromDB.cs
+++ ServerTest/Database/ResponceFromDB.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
+using ServerTest.Server.ServerInterface;
 
 namespace ServerTest.Database
 {
@@ -19,10 +20,14 @@ namespace ServerTest.Database
                 case 1062:
                     Responce = "Username or email already used.";
                     break;
+                //case 1032:
+                //    Responce = "Can't find recordy by SELECT request";
+                //    break;
                 default:
                     Responce = "There aren't any exceptions!";
                     break;
             }
+            FormsManaging.TextGenerator(Responce);
             return Responce;
         }
     }
