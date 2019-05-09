1b67e4fcc2939e5d6525d66c3123197ab1f98625
Elleyer <shudenkoslavik@gmail.com>
2019-05-09 14:02:27 +0300

    Update Starter func

--- NetCoreServer/Starter.cs
+++ NetCoreServer/Starter.cs
@@ -8,7 +8,10 @@ namespace NetCoreServer
         static void Main(string[] args)
         {
             Connector.ServerStart();
-            Console.ReadKey();
+            while (true)
+            {
+             Console.ReadKey();
+            }
         }
     }
 }
