a1686252e3df811e2f2926150113c14d596910c4
Elleyer <shudenkoslavik@gmail.com>
2019-06-01 19:28:06 +0300

    Update connector (List<Clients>)

--- NetCoreServer/Server/Connector/Connector.cs
+++ NetCoreServer/Server/Connector/Connector.cs
@@ -6,45 +6,103 @@ using System.Net.Sockets;
 using System.Net;
 using System.Text;
 using NetCoreServer.Database;
-using NetCoreServer.Server.ServerInterface;
+using NetCoreServer.ServerInterface;
 
 namespace NetCoreServer.Server.Connector
 {
     class Connector
     {
         public const int PORT_NO = 27015;
-        public const string SERVER_IP = "127.0.0.1";
+        public const string SERVER_IP = "192.168.0.106";
         public static TcpListener listener;
+        public static List<TcpClient> Clients;
         public static TcpClient client;
 
-        public static async void ServerStart()
+
+        public async void ServerStart()
         {
+            Clients = new List<TcpClient>(100000);
+            ID.playerID = new List<int>(100000);
+            for (int i = 0; i < Clients.Capacity; i++)
+            {
+                Clients.Add(null);
+            }
             FormsManaging.TextGenerator("Server started.");
+            DatabaseConnector.ConnectToDB();
             IPAddress localAdd = IPAddress.Any;
             listener = new TcpListener(localAdd, PORT_NO);
-            FormsManaging.TextGenerator("Listening...");
-            //RequestToDB.CreateRequest("ALTER TABLE users ADD regTime TIMESTAMP DEFAULT NOW()");
+            listener.Start();
 
             while (true)
             {
-                 await SyncTask();         
+                 await SyncTask();
             }
         }
 
-        public static async Task SyncTask()
+        private async Task SyncTask()
         {
-            Task sync = new Task(action: listener.Start);
-            listener.Start();
             client = await listener.AcceptTcpClientAsync();
-            FormsManaging.TextGenerator("Synced");
-            FormsManaging.TextGenerator(client.Client.RemoteEndPoint.ToString());
-            NetworkStream nwStream = client.GetStream();
-            byte[] bufferreceive = new byte[client.ReceiveBufferSize];
-            int bytesRead = nwStream.Read(bufferreceive, 0, client.ReceiveBufferSize);
-            string dataReceived = Encoding.ASCII.GetString(bufferreceive,0,bytesRead);
-            ClientAction.Action(dataReceived);
-            client.Close();
-            listener.Stop();
+
+            if (!Clients.Contains(client))
+            {
+                var id = ID.GenerateUniqueId();
+                FormsManaging.TextGenerator("Created list!");
+                Clients.Insert(id,client);
+                ID.idCount++;
+                await ConnectFirst(id);
+            }
+        }
+
+        private async Task AlreadyConnected(int id)
+        {
+            if (Clients.Contains(client))
+            {
+                FormsManaging.TextGenerator("Already exists");
+                await SynchronizationUser(id);
+            }
+        }
+
+        private async Task SynchronizationUser(int id)
+        {
+            FormsManaging.TextGenerator("Synced as already connected user!");
+            NetworkStream nwStream = Clients[id].GetStream();
+            byte[] bufferReceive = new byte[Clients[id].ReceiveBufferSize];
+            int bytesRead = nwStream.Read(bufferReceive, 0, Clients[id].ReceiveBufferSize);
+            string dataReceived = Encoding.ASCII.GetString(bufferReceive,0,bytesRead);
+            CreateAction(dataReceived, id);
+
+            if (Clients[id].Connected)
+            {
+                await AlreadyConnected(id);
+            }
+            else
+            {
+                Clients[id].Client.Dispose();
+            }
+        }
+
+        private async Task ConnectFirst(int id)
+        {
+            FormsManaging.TextGenerator("Synced as new user with id " + id);
+            NetworkStream nwStream = Clients[id].GetStream();
+            byte[] bufferReceive = new byte[Clients[id].ReceiveBufferSize];
+            int bytesRead = nwStream.Read(bufferReceive, 0, Clients[id].ReceiveBufferSize);
+            string dataReceived = Encoding.ASCII.GetString(bufferReceive,0,bytesRead);
+            CreateAction(dataReceived, id);
+            if (Clients[id].Connected)
+            {
+                await AlreadyConnected(id);
+            }
+            else
+            {
+                Clients[id].Client.Dispose();
+            }
+        }
+
+        private void CreateAction(string receivedData, int id)
+        {
+            var action = new ClientAction();
+            action.Action(receivedData,id);
         }
     }
 }
