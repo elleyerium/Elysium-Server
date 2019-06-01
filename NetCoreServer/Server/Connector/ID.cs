b207c3b8525240997c098eda6576e3c15b58886c
Elleyer <shudenkoslavik@gmail.com>
2019-06-01 19:27:37 +0300

    ID generator for server

---
+++ NetCoreServer/Server/Connector/ID.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using NetCoreServer.ServerInterface;
+
+namespace NetCoreServer.Server.Connector
+{
+    public static class ID
+    {
+        public static int idCount = -1;
+        public static List<int> playerID;
+
+        public static int GenerateUniqueId()
+        {
+            var value = RandomizeId();
+            if (!playerID.Contains(value))
+            {
+                playerID.Add(value);
+                //return value;
+            }
+            else if (!playerID.Contains(value))
+            {
+                GenerateUniqueId();
+            }
+            FormsManaging.TextGenerator(value.ToString());
+            return value;
+        }
+
+        private static int RandomizeId()
+        {
+            Random random = new Random();
+            var value = random.Next(0, 100000);
+            return value;
+        }
+    }
+}
