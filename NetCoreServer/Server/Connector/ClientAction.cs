9d0d0e5fbe886b754f2a1e4d51a58d4255695be0
Elleyer <shudenkoslavik@gmail.com>
2019-10-24 01:21:48 +0300

    New serialization type.

--- NetCoreServer/Server/Connector/ClientAction.cs
+++ NetCoreServer/Server/Connector/ClientAction.cs
@@ -1,57 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NetCoreServer.Database;
 using NetCoreServer.Database.DataTypes;
 using NetCoreServer.Server.Auth;
+using NetCoreServer.Server.Multiplayer.Leaderboards;
+using NetCoreServer.Server.User;
 using NetCoreServer.Server.User.PlayerStatistics;
-using NetCoreServer.Server.User.Leaderboards;
 using NetCoreServer.ServerInterface;
-
 namespace NetCoreServer.Server.Connector
 {
     class ClientAction
     {
-        public void Action(string ReceivedData, int id)
+        public void Action(byte[] receivedData, Connector connector, int id)
         {
-            string[] DataArray = ReceivedData.Split('|');
-            string request = DataArray.Last();
-            string Tag = DataArray.First();
-            try
+            using (var reader = new BinaryReader(new MemoryStream(receivedData)))
             {
-                switch (Tag)
+                try
                 {
-                    case "RegistrationRequest":
-                        Register.RegisterProfile(DataArray.Last(), id);
-                        Scores.CreateTable(Items.GetScoreList(request), id);
-                        break;
-                    case "LoginRequest":
-                        Login.Auth(DataArray.Last(),id);
-                        break;
-                    case "SetScoreRequest":
-                        Scores.SetUserScores(request, id);
-                        break;
-                    case "GetScoreRequest":
-                        Scores.GetUserScores(Items.GetScoreList(request),id);
-                        break;
-                    case "GetPlayerStatsPosition":
-                        break;
-                    case "GetLeaderboardsRequest":
-                        MainLeaderboard.RequestLeaderboards(id);
-                        break;
-                    case "GetClientID" :
-                        ServerResponces.SendResponse(Connector.Clients[id], id.ToString());
-                        break;
-                    default:
-                        throw new Exception(Tag);
+                    var first = reader.ReadByte();
+                    switch (first)
+                    {
+                        case 1:
+                            var uname = reader.ReadString();
+                            //Login.Auth(uname, id);
+                            //connector.Holder.List.Add(new Player(new PlayerAccountInfo(uname), new PlayerMultiplayerInfo()));
+                            break;
+                        case 5:
+                            Register.RegisterProfile(reader.ReadString(), id);
+                            Scores.CreateTable(Items.GetScoreList(reader.ReadString()), id);
+                            break;
+                        /*case "GetScoreRequest":
+                            Scores.GetUserScores(Items.GetScoreList(request), id);
+                            break;
+                        case "GetPlayerStatsPosition":
+                            break;
+                        case "GetLeaderboardsRequest":
+                            MainLeaderboard.RequestLeaderboards(id);
+                            break;
+                        case "GetClientID":
+                            ServerResponces.SendResponse(connector.Clients[id], id.ToString());
+                            break;*/
+                        default:
+                            throw new Exception($"Unknown byte {first}");
+                    }
                 }
-            }
-            catch (Exception ex)
-            {
-                FormsManaging.TextGenerator(ex.ToString());
+                catch (Exception ex)
+                {
+                    FormsManaging.TextGenerator(ex.ToString());
+                }
             }
         }
     }
--- NetCoreServer/Server/Connector/Connector.cs
+++ NetCoreServer/Server/Connector/Connector.cs
@@ -1,108 +1,115 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Net.Sockets;
 using System.Net;
 using System.Text;
+using System.Threading;
+using MySql.Data.MySqlClient;
 using NetCoreServer.Database;
+using NetCoreServer.Server.User;
 using NetCoreServer.ServerInterface;
 
 namespace NetCoreServer.Server.Connector
 {
-    class Connector
+    internal class Connector
     {
-        public const int PORT_NO = 27015;
-        public const string SERVER_IP = "192.168.0.106";
-        public static TcpListener listener;
-        public static List<TcpClient> Clients;
-        public static TcpClient client;
+        private const int Port = 27015;
+        private const string ServerAddress = "127.0.0.1";
+        private TcpListener _listener;
+        public List<Player> Players;
+        public PlayersHolder Holder;
+        public DatabaseProvider Provider;
+        private TcpClient client, thisClient;
 
 
-        public async void ServerStart()
+        public async Task ServerStart()
         {
-            Clients = new List<TcpClient>(100000);
-            ID.playerID = new List<int>(100000);
-            for (int i = 0; i < Clients.Capacity; i++)
-            {
-                Clients.Add(null);
-            }
+            Holder = new PlayersHolder();
+            Players = new List<Player>();
+            Provider = new DatabaseProvider(new ResponseFromDB(), new RequestToDB(), new MySqlConnection());
             FormsManaging.TextGenerator("Server started.");
-            DatabaseConnector.ConnectToDB();
-            IPAddress localAdd = IPAddress.Any;
-            listener = new TcpListener(localAdd, PORT_NO);
-            listener.Start();
+            var localAdd = IPAddress.Any;
+            _listener = new TcpListener(localAdd, Port);
+            _listener.Start();
 
             while (true)
             {
-                 await SyncTask();
+                await SyncTask();
             }
         }
 
         private async Task SyncTask()
         {
-            client = await listener.AcceptTcpClientAsync();
-
-            if (!Clients.Contains(client))
-            {
-                var id = ID.GenerateUniqueId();
-                FormsManaging.TextGenerator("Created list!");
-                Clients.Insert(id,client);
-                ID.idCount++;
-                await ConnectFirst(id);
-            }
+            client = await _listener.AcceptTcpClientAsync();
+            SynchronizationUser(new Guid());
         }
 
-        private async Task AlreadyConnected(int id)
+        private void SynchronizationUser(Guid guid)
         {
-            if (Clients.Contains(client))
-            {
-                FormsManaging.TextGenerator("Already exists");
-                await SynchronizationUser(id);
-            }
-        }
+            thisClient = client;
+            FormsManaging.TextGenerator("New client available!");
+            /*var nwStream = Players.FirstOrDefault(x => x.)GetStream();
+            var bufferReceive = new byte[Players[id].Client.ReceiveBufferSize];
+            var bytesRead = nwStream.Read(bufferReceive, 0, Players[id].ReceiveBufferSize);
+            var action = CreateAction(bufferReceive, id);*/ //TODO: fix
 
-        private async Task SynchronizationUser(int id)
-        {
-            FormsManaging.TextGenerator("Synced as already connected user!");
-            NetworkStream nwStream = Clients[id].GetStream();
-            byte[] bufferReceive = new byte[Clients[id].ReceiveBufferSize];
-            int bytesRead = nwStream.Read(bufferReceive, 0, Clients[id].ReceiveBufferSize);
-            string dataReceived = Encoding.ASCII.GetString(bufferReceive,0,bytesRead);
-            CreateAction(dataReceived, id);
-
-            if (Clients[id].Connected)
+            /*if (Clients[id].Connected)
             {
                 await AlreadyConnected(id);
             }
             else
             {
-                Clients[id].Client.Dispose();
-            }
+                Clients.Remove(Clients[id]);
+            }*/
         }
 
-        private async Task ConnectFirst(int id)
+        private void ReturnImage()
         {
-            FormsManaging.TextGenerator("Synced as new user with id " + id);
-            NetworkStream nwStream = Clients[id].GetStream();
-            byte[] bufferReceive = new byte[Clients[id].ReceiveBufferSize];
-            int bytesRead = nwStream.Read(bufferReceive, 0, Clients[id].ReceiveBufferSize);
-            string dataReceived = Encoding.ASCII.GetString(bufferReceive,0,bytesRead);
-            CreateAction(dataReceived, id);
-            if (Clients[id].Connected)
-            {
-                await AlreadyConnected(id);
-            }
-            else
-            {
-                Clients[id].Client.Dispose();
-            }
-        }
+            var fs = new FileStream(@"Resources/saitama.jpg", FileMode.Open, FileAccess.Read );
 
-        private void CreateAction(string receivedData, int id)
+        }
+        private void CreateAction(byte[] data, int id)
         {
             var action = new ClientAction();
-            action.Action(receivedData,id);
+            action.Action(data, this, id);
+        }
+
+        public void BroadcastData()
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                using (var writer = new BinaryWriter(memoryStream))
+                {
+
+                }
+            }
+
         }
     }
 }
+
+/*private async Task AlreadyConnected(int id)
+{
+    if (Players.Contains(client))
+    {
+        FormsManaging.TextGenerator("Already exists");
+        await SynchronizationUser(id);
+    }
+}*/
+/*private async Task ConnectFirst(int id)
+{
+    FormsManaging.TextGenerator("Synced as new user with id " + id);
+    NetworkStream nwStream = Clients[id].GetStream();
+    byte[] bufferReceive = new byte[Clients[id].ReceiveBufferSize];
+    int bytesRead = nwStream.Read(bufferReceive, 0, Clients[id].ReceiveBufferSize);
+    string dataReceived = Encoding.ASCII.GetString(bufferReceive,0,bytesRead);
+    CreateAction(dataReceived, id);
+    if (Clients[id].Connected)
+        await AlreadyConnected(id);
+    else Clients[id].Client.Dispose();
+}
+
+} */
