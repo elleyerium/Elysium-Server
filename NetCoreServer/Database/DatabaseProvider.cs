bf2c0a256a05de16803e6f1aca960543bea3fb2a
Elleyer <shudenkoslavik@gmail.com>
2019-10-28 10:26:49 +0200

    Update DatabaseProvider.cs

--- NetCoreServer/Database/DatabaseProvider.cs
+++ NetCoreServer/Database/DatabaseProvider.cs
@@ -1,23 +1,74 @@
+using System.IO;
 using MySql.Data.MySqlClient;
+using NetCoreServer.Server.Connector;
+using NetCoreServer.ServerInterface;
 
 namespace NetCoreServer.Database
 {
     public class DatabaseProvider
     {
+        internal ConnectionProvider ConnectionProv;
         internal ResponseFromDB ResponceFromDb;
-        internal RequestToDB RequestToDb;
         internal MySqlConnection SqlConnection;
 
-        public DatabaseProvider(ResponseFromDB responseInstance, RequestToDB requestInstance, MySqlConnection sqlConnection)
+        public DatabaseProvider(ResponseFromDB responseInstance, MySqlConnection sqlConnection)
         {
             ResponceFromDb = responseInstance;
-            RequestToDb = requestInstance;
             SqlConnection = sqlConnection;
         }
 
+        /*public byte[] Request(string message)
+        {
+            return RequestToDb.CreateRequest(this, message);
+        }*/
         public void ConnectToDB()
         {
             SqlConnection = new MySqlConnection("Server=localhost;Database=database;Uid=root;Pwd=34etehuh121;");
+            FormsManaging.TextGenerator("Connected to a Database.");
+        }
+
+        public static byte[] CreateRequest(DatabaseProvider databaseProvider, string requestMessage)
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                FormsManaging.TextGenerator($"Created a new request with data :> {requestMessage}");
+                using (var writer = new BinaryWriter(memoryStream))
+                {
+                    try
+                    {
+                        var sqlCommand = databaseProvider.SqlConnection.CreateCommand();
+                        sqlCommand.CommandText = requestMessage;
+                        databaseProvider.SqlConnection.Open();
+                        var reader = sqlCommand.ExecuteReader();
+                        reader.Read();
+
+                        if (reader.HasRows)
+                            writer.Write((byte)1);
+                        else if (!reader.HasRows)
+                            writer.Write((byte)0);
+                        //writer.Write((byte)6);
+                        /*var dataAffected = reader.RecordsAffected;
+                        if (dataAffected > 0)
+                        {
+                            writer.Write((byte)0);
+                            FormsManaging.TextGenerator("Registered");
+                        }
+                        else if (dataAffected == 0)
+                        {
+                            writer.Write((byte)1);
+                            FormsManaging.TextGenerator("failed");
+                        }*/
+
+                    }
+                    catch (MySqlException exception)
+                    {
+                        FormsManaging.TextGenerator(exception.ToString());
+                    }
+                }
+                databaseProvider.SqlConnection.Close();
+                //FormsManaging.TextGenerator(memoryStream.Length.ToString());
+                return memoryStream.ToArray();
+            }
         }
     }
 }
