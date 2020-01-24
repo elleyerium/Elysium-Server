7979ae845fb5169158afa8c301010f4a72c90c1c
Elleyer <shudenkoslavik@gmail.com>
2020-01-25 01:20:54 +0200

    Update DatabaseProvider.cs

--- NetCoreServer/Database/DatabaseProvider.cs
+++ NetCoreServer/Database/DatabaseProvider.cs
@@ -1,6 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
 using System.IO;
 using MySql.Data.MySqlClient;
 using NetCoreServer.Server.Connector;
+using NetCoreServer.Server.User.PlayerStatistics;
 using NetCoreServer.ServerInterface;
 
 namespace NetCoreServer.Database
@@ -8,67 +12,120 @@ namespace NetCoreServer.Database
     public class DatabaseProvider
     {
         internal ConnectionProvider ConnectionProv;
-        internal ResponseFromDB ResponceFromDb;
         internal MySqlConnection SqlConnection;
 
-        public DatabaseProvider(ResponseFromDB responseInstance, MySqlConnection sqlConnection)
+        public DatabaseProvider(MySqlConnection sqlConnection)
         {
-            ResponceFromDb = responseInstance;
             SqlConnection = sqlConnection;
         }
 
-        /*public byte[] Request(string message)
-        {
-            return RequestToDb.CreateRequest(this, message);
-        }*/
-        public void ConnectToDB()
+        public void Connect()
         {
             SqlConnection = new MySqlConnection("Server=localhost;Database=database;Uid=root;Pwd=12345678;");
-            FormsManaging.TextGenerator("Connected to a Database.");
+            FormsManaging.TextGenerator("Connected to the Database.");
         }
 
-        public static byte[] CreateRequest(DatabaseProvider databaseProvider, string requestMessage)
+        private MySqlDataReader CreateCommand(string data)
         {
-            using (var memoryStream = new MemoryStream())
+            if(SqlConnection.State == ConnectionState.Open)
+                SqlConnection.Close();
+            SqlConnection.Open();
+            var command = SqlConnection.CreateCommand();
+            command.CommandText = data;
+            return command.ExecuteReader();
+        }
+
+        public bool AuthenticateUser(string username, string salt)
+        {
+            var reader = CreateCommand($"select * from users where username = '{username}' AND password = '{salt}'");
+            while (reader.Read())
             {
-                FormsManaging.TextGenerator($"Created a new request with data :> {requestMessage}");
-                using (var writer = new BinaryWriter(memoryStream))
-                {
-                    try
-                    {
-                        var sqlCommand = databaseProvider.SqlConnection.CreateCommand();
-                        sqlCommand.CommandText = requestMessage;
-                        databaseProvider.SqlConnection.Open();
-                        var reader = sqlCommand.ExecuteReader();
-                        reader.Read();
-
-                        if (reader.HasRows)
-                            writer.Write((byte)1);
-                        else if (!reader.HasRows)
-                            writer.Write((byte)0);
-                        //writer.Write((byte)6);
-                        /*var dataAffected = reader.RecordsAffected;
-                        if (dataAffected > 0)
-                        {
-                            writer.Write((byte)0);
-                            FormsManaging.TextGenerator("Registered");
-                        }
-                        else if (dataAffected == 0)
-                        {
-                            writer.Write((byte)1);
-                            FormsManaging.TextGenerator("failed");
-                        }*/
-
-                    }
-                    catch (MySqlException exception)
-                    {
-                        FormsManaging.TextGenerator(exception.ToString());
-                    }
-                }
-                databaseProvider.SqlConnection.Close();
-                //FormsManaging.TextGenerator(memoryStream.Length.ToString());
-                return memoryStream.ToArray();
+                if (reader.HasRows)
+                    return true;
             }
+            return false;
+        }
+
+        public bool RegisterAccount(string username, string password, string email, string token)
+        {
+            var reader = CreateCommand(
+                $"insert into users (username, password, email, token) values ('{username}', '{password}', '{email}', '{token}')");
+            while (reader.Read())
+            {
+                Console.WriteLine("Affected");
+            }
+            return reader.RecordsAffected > 0;
+        }
+
+        public int GetIdByUsername(string username)
+        {
+            var reader = CreateCommand($"select id from users where username = '{username}'");
+            var id = 0;
+
+            while (reader.Read())
+            {
+                id = int.Parse(reader["id"].ToString());
+            }
+
+            return id;
+        }
+
+        #region Tokens
+
+        public IEnumerable<string> GetTokens()
+        {
+            var list = new List<string>();
+            var reader = CreateCommand("select token from users");
+
+            while (reader.Read())
+            {
+                list.Add(reader["token"].ToString());
+            }
+            return list.ToArray();
+        }
+
+        public bool IsTokenValid(string token)
+        {
+            var reader = CreateCommand($"select token from users where token = '{token}'");
+            while (reader.Read())
+            {
+                if (reader.HasRows)
+                    return true;
+            }
+            return false;
+        }
+
+        public void RemoveToken(string username)
+        {
+            var reader = CreateCommand($"update users set token = null where username = '{username}'");
+        }
+
+        public string GetTokenByName(string username)
+        {
+            var reader = CreateCommand($"select token from users where username = '{username}'");
+            string token = null;
+            while (reader.Read())
+            {
+                token = reader["token"].ToString();
+            }
+
+            return token;
+        }
+
+        #endregion
+
+        public PlayerMultiplayerInfo GetInfoByUsername(string username)
+        {
+            var info = new PlayerMultiplayerInfo();
+            var reader = CreateCommand($"select score, level, spacepoints from scores where username = '{username}'");
+            while (reader.Read())
+            {
+                info.Score = int.Parse(reader["score"].ToString());
+                info.Level = int.Parse(reader["level"].ToString());
+                info.SpacePoints = int.Parse(reader["spacepoints"].ToString());
+            }
+            Console.WriteLine($"score: {info.Score}, level: {info.Level}, SP: {info.SpacePoints}");
+            return info;
         }
     }
 }
