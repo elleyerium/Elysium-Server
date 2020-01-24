a52e973531e5ed97954068a4f9e25641b51b82c2
Elleyer <shudenkoslavik@gmail.com>
2020-01-25 01:20:29 +0200

    Create ServerInfo.cs

---
+++ NetCoreServer/Server/Statistics/ServerInfo.cs
@@ -0,0 +1,9 @@
+namespace NetCoreServer.Server.Statistics
+{
+    public class ServerInfo
+    {
+        public int ConnectionsCount { get; set; }
+        public int RoomsCount { get; set; }
+        public const string ClientVersion = "1.12b";
+    }
+}
